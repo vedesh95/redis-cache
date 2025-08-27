f4cabac552dc378c9de7709f66b4ba1709a0f235
vedesh95 <vedesh95@gmail.com>
2025-08-27 23:38:53 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
@@ -98,7 +99,7 @@ public class Main {
                         for(int i = 2; i < command.size(); i++){
                             String value = command.get(i);
                             if(!lists.containsKey(key)){
-                                lists.put(key, new java.util.ArrayList<>());
+                                lists.put(key, new ArrayList<>());
                             }
                             lists.get(key).add(value);
                         }
@@ -136,7 +137,7 @@ public class Main {
                         for(int i = 2; i < command.size(); i++){
                             String value = command.get(i);
                             if(!lists.containsKey(key)){
-                                lists.put(key, new java.util.ArrayList<>());
+                                lists.put(key, new ArrayList<>());
                             }
                             lists.get(key).add(0, value);
                         }
@@ -177,7 +178,7 @@ public class Main {
                                 for(String value : list){
                                     out.write(("$" + value.length() + "\r\n" + value + "\r\n").getBytes());
                                 }
-                                lists.put(key, new java.util.ArrayList<>());
+                                lists.put(key, new ArrayList<>());
                                 out.flush();
                             } else {
                                 // if count == 1 then return a bulk string
@@ -214,9 +215,11 @@ public class Main {
                         if (threadsWaitingForBLPOP.containsKey(key) == false) {
                             threadsWaitingForBLPOP.put(key, new ConcurrentLinkedQueue<>());
                         }
-                        threadsWaitingForBLPOP.get(key).add(currentThread);
+                        threadsWaitingForBLPOP.get(key).offer(currentThread);
+                        System.out.println("----blpop----" + key + " " + timeout + " " + currentThread + threadsWaitingForBLPOP.get(key).size());
                         long startTime = System.currentTimeMillis();
                         while (waitForever || (System.currentTimeMillis() - startTime) < timeout && threadsWaitingForBLPOP.get(key).peek() == currentThread) {
+
                             if (lists.containsKey(key) && lists.get(key).isEmpty() == false) {
                                 String value = lists.get(key).remove(0);
                                 out.write(("*2\r\n$" + key.length() + "\r\n" + key + "\r\n" + "$" + value.length() + "\r\n" + value + "\r\n").getBytes());
