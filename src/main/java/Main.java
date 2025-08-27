2577aad8ab4736d72acffe16d2048b9fbfe43f18
vedesh95 <vedesh95@gmail.com>
2025-08-28 00:42:12 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -273,8 +273,7 @@ public class Main {
                             streamMap.get(streamkey).add(new KeyValue(key, value));
                         }
                         // The return value is the ID of the entry created as a bulk string.
-                        String id = String.valueOf(streamMap.get(streamkey).size());
-                        System.out.println("-----debug=" + streamkey);
+                        String id = String.valueOf(streamMap);
                         out.write(("$" + id.length() + "\r\n" + id + "\r\n").getBytes());
                         out.flush();
                     }
