57180cfffbf4a6b30646cec23eb46edd7dd0f0ca
vedesh95 <vedesh95@gmail.com>
2025-08-29 22:51:30 +0530

    [any message]

--- src/main/java/command/Blpop.java
+++ src/main/java/command/Blpop.java
@@ -23,19 +23,47 @@ public class Blpop implements Command {
         this.streamMap = streamMap;
     }
 
+//    @Override
+//    public void execute(List<String> command, OutputStream out) throws IOException {
+//        String key = command.get(1);
+//        double timeout = Double.parseDouble(command.get(2)) * 1000; // convert to milliseconds
+//        boolean waitForever = timeout == 0;
+//        if (!threadsWaitingForBLPOP.containsKey(key)) {
+//            threadsWaitingForBLPOP.put(key, new ConcurrentLinkedQueue<>());
+//        }
+//        threadsWaitingForBLPOP.get(key).offer(Thread.currentThread());
+//        long startTime = System.currentTimeMillis();
+//        boolean found = false;
+//        while (waitForever || (System.currentTimeMillis() - startTime) < timeout) {
+//            if(threadsWaitingForBLPOP.get(key).peek() == Thread.currentThread()){
+//                if (lists.containsKey(key) && !lists.get(key).isEmpty()) {
+//                    String value = lists.get(key).remove(0);
+//                    out.write(("*2\r\n$" + key.length() + "\r\n" + key + "\r\n" + "$" + value.length() + "\r\n" + value + "\r\n").getBytes());
+//                    out.flush();
+//                    threadsWaitingForBLPOP.get(key).remove(Thread.currentThread());
+//                    found = true;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        if(!found){
+//            out.write("*-1\r\n".getBytes());
+//            out.flush();
+//            threadsWaitingForBLPOP.get(key).remove(Thread.currentThread());
+//        }
+//    }
+
     @Override
     public void execute(List<String> command, OutputStream out) throws IOException {
         String key = command.get(1);
         double timeout = Double.parseDouble(command.get(2)) * 1000; // convert to milliseconds
         boolean waitForever = timeout == 0;
-        if (!threadsWaitingForBLPOP.containsKey(key)) {
-            threadsWaitingForBLPOP.put(key, new ConcurrentLinkedQueue<>());
-        }
-        threadsWaitingForBLPOP.get(key).offer(Thread.currentThread());
+
         long startTime = System.currentTimeMillis();
         boolean found = false;
         while (waitForever || (System.currentTimeMillis() - startTime) < timeout) {
-            if(threadsWaitingForBLPOP.get(key).peek() == Thread.currentThread()){
+            synchronized (lists){
                 if (lists.containsKey(key) && !lists.get(key).isEmpty()) {
                     String value = lists.get(key).remove(0);
                     out.write(("*2\r\n$" + key.length() + "\r\n" + key + "\r\n" + "$" + value.length() + "\r\n" + value + "\r\n").getBytes());
