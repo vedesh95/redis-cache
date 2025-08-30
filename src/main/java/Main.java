fcefb8fc7810e8e54f1d93c5928582dde9a50410
vedesh95 <vedesh95@gmail.com>
2025-08-30 21:30:01 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -59,7 +59,7 @@ public class Main {
                 cache.getInfo().setRole("slave");
                 String line = reader.readLine();
                 if(line!=null && line.charAt(0)=='$'){
-                    for(int i=0;i<8;i++) reader.readLine();
+                    for(int i=0;i<8;i++) System.out.println(reader.readLine());
                     slave.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n".getBytes());
                     slave.getOutputStream().flush();
                 }
