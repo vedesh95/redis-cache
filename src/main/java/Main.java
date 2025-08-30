0b0acb59b05fd9ff22c51a38e8a72e75bc55f7e9
vedesh95 <vedesh95@gmail.com>
2025-08-30 15:47:29 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,7 +68,11 @@ public class Main {
 //                    slave.getOutputStream().flush();
 //                }
                 // reader can contains bulk strings now. read string from reader until null
-                readBulkStrings(reader);
+                try{
+                    readBulkStrings(reader);
+                }catch (IOException e){
+                    System.out.println(e);
+                }
                 slave.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n".getBytes());
                 slave.getOutputStream().flush();
 //                cache.addClient(slave);
