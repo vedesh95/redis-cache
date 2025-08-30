5b74dcc87e653f3739fdca2b2d347bd7b4e1605a
vedesh95 <vedesh95@gmail.com>
2025-08-30 11:00:32 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -54,6 +54,7 @@ public class Main {
                 // check response
                 response = reader.readLine();
                 cache.getInfo().setRole("slave");
+                slave.close();
             }catch (Exception e){
                 System.out.println("Failed to connect to master: " + e.getMessage());
                 return;
