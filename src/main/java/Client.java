ffdf30e434e87fd179081a6a48112162a91f7eb5
vedesh95 <vedesh95@gmail.com>
2025-08-31 14:56:19 +0530

    [any message]

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -119,7 +119,7 @@ public class Client {
 //                        slaveOut.flush();
                         if(!socket.isClosed() && socket.isConnected()){
                             socket.getOutputStream().write(("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n").getBytes());
-                            socket.getOutputStream().flush();s
+                            socket.getOutputStream().flush();
                             System.out.println("Sent REPLCONF GETACK * to slave: " + socket);
                         }
 
