5e9bd8538da60cb4383dfe2a5e10075c9c798a4c
vedesh95 <vedesh95@gmail.com>
2025-08-31 15:04:48 +0530

    [any message]

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -121,7 +121,7 @@ public class Client {
 //                            System.out.println("Trying to Send REPLCONF GETACK * to slave: " + socket);
                             socket.getOutputStream().write(("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n").getBytes());
                             socket.getOutputStream().flush();
-//                            System.out.println("Sent REPLCONF GETACK * to slave: " + socket);
+                            System.out.println("Sent REPLCONF GETACK * to slave: " + socket);
                         }
 
 //                        System.out.println("Sent REPLCONF GETACK * to slave: " + socket);
