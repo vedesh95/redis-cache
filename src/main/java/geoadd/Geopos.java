e7e9181ce44bcbec480389dddc3d4f219835a879
vedesh95 <vedesh95@gmail.com>
2025-09-02 21:28:18 +0530

    [any message]

--- src/main/java/geoadd/Geopos.java
+++ src/main/java/geoadd/Geopos.java
@@ -6,6 +6,7 @@ import struct.SortedSet;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Geopos implements GeoCommand {
@@ -15,20 +16,25 @@ public class Geopos implements GeoCommand {
     }
     @Override
     public void execute(List<String> command, OutputStream out, Socket socket) throws IOException {
-        String key = command.get(1);
-        String member = command.get(2);
-        double score = this.sortedSet.getZScore(key, member);
-        if(score == -1){
-            out.write(("*2\r\n*-1\r\n*-1\r\n").getBytes());
-            out.flush();
-            return;
+        List<Double> coords = new ArrayList<>();
+        for(int i=0;i<command.size();i=i+2){
+            String key = command.get(1);
+            String member = command.get(2);
+            double score = this.sortedSet.getZScore(key, member);
+            if(score == -1){
+                out.write(("*2\r\n*-1\r\n*-1\r\n").getBytes());
+                out.flush();
+                return;
+            }
+            coords.addAll(RedisGeoCodec.decode((long) score));
         }
-        List<Double> coords = RedisGeoCodec.decode((long) score);
-        out.write(("*2\r\n".getBytes()));
-        out.write(("$" + String.valueOf(coords.get(1)).length() + "\r\n").getBytes());
-        out.write((String.valueOf(coords.get(1)) + "\r\n").getBytes());
-        out.write(("$" + String.valueOf(coords.get(0)).length() + "\r\n").getBytes());
-        out.write((String.valueOf(coords.get(0)) + "\r\n").getBytes());
+        // output
+        StringBuilder sb = new StringBuilder();
+        sb.append("*").append(coords.size()).append("\r\n");
+        for(double coord : coords){
+            sb.append("$").append(String.valueOf(coord).length()).append("\r\n").append(coord).append("\r\n");
+        }
+        out.write(sb.toString().getBytes());
         out.flush();
     }
 }
